83da3b7befbddb2f4ee2eb8e133a47ad7e499189
MangooH <hmk404@yonsei.ac.kr>
2024-01-02 15:29:05 +0900

    eda tut

--- back.py
+++ back.py
@@ -1,24 +1,3 @@
-s = input()
+import pandas as pd
 
-operator_list = []
-minus_idx = 0
-for i, e in enumerate(s):
-    if e == "-" and not minus_idx:
-        minus_idx = i
-    if e == "+" or e == "-":
-        operator_list.append(i)
-operator_list.append(len(s))
-
-flag = False
-idx = 0
-result = 0
-for i in operator_list:
-    if flag:
-        result -= int(s[idx:i])
-    else:
-        result += int(s[idx:i])
-    if i == minus_idx:
-        flag = True
-    idx = i + 1
-
-print(result)
+print(pd.__version__)
